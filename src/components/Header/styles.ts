1a0235f82ab667e0be9595075b4f52aef33608ac
Gustavo <gustavosm994@gmail.com>
2022-12-13 15:06:15 -0300

    feat(header): add header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  background: ${({ theme }) => theme['gray-900']};
+  padding: 2.5rem 0 7.5rem;
+`
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const NewTransactionButton = styled.button`
+  height: 48px;
+  border: 0;
+  background: ${({ theme }) => theme['green-500']};
+  color: ${({ theme }) => theme.white};
+  font-weight: 700;
+  padding: 0 1.25rem;
+  border-radius: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background: ${({ theme }) => theme['green-700']};
+    transition: background-color 0.2s;
+  }
+`
